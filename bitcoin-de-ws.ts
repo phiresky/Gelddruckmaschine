61674c0ab8c2a9e7ad1c8fb99774edd97b1c8a1d
phiresky <phireskyde+git@gmail.com>
2017-08-19 01:58:55 +0200

    loop money

--- bitcoin-de-ws.ts
+++ bitcoin-de-ws.ts
@@ -1,30 +1,28 @@
 import { Websocket_API } from "./generated";
 
 const io = require("socket.io-client");
-var socket = io.connect('https://ws.bitcoin.de', {port: 443});
+var socket = io.connect("https://ws.bitcoin.de", { port: 443 });
 
-socket.on('connect', function () {
+socket.on("connect", function() {
 	// Connection has successfully been established.
 	// Do some initialization stuff here.c
-	console.log("connected");
+	// console.log("connected");
 });
 
-socket.on('disconnect', function () {
-});
+socket.on("disconnect", function() {});
 
 // An order has been removed from the orderbook
-socket.on('remove_order', function (order: Websocket_API.add_order) {
+socket.on("remove_order", function(order: Websocket_API.add_order) {
 	// Variable order contains the element order_id
 	//console.log("remove", order);
 });
 
 // A new order has been added to the orderbook
-socket.on('add_order', function (order: Websocket_API.add_order) {
+socket.on("add_order", function(order: Websocket_API.add_order) {
 	// Variable order contains all above listed elements (i.e. order_id, order_type)
 	//console.log("add", order);
 });
 
-
 export function onBitcoindeOrderCreated(callback: (order: Websocket_API.add_order) => void) {
-	socket.on('add_order', callback);
+	socket.on("add_order", callback);
 }
--- printer.ts
+++ printer.ts
@@ -78,10 +78,10 @@ async function updateKrakenPrice() {
 		}
 		krakenPrice_EUR = formattedResult[formattedResult.length - 1].price_EUR;
 		debug(`Fetched new Kraken price: ${krakenPrice_EUR} EUR (from ${lastTime})`);
-    } catch (error) {
-        if (error.statusCode) {
-            error = `Status Code: ${error.statusCode}`;
-        }
+	} catch (error) {
+		if (error.statusCode) {
+			error = `Status Code: ${error.statusCode}`;
+		}
 		krakenPrice_EUR = "unknown";
 		console.warn(`Could not fetch new Kraken prices - setting 'unknown'. Error: ${error}`);
 	}
@@ -99,13 +99,7 @@ function getProfitMargin(krakenPrice_EURperBTC: number, btcdePrice_EURperBTC: nu
 	return (krakenPriceWithFees_EURperBTC - btcdePriceWithFees_EURperBTC) / btcdePriceWithFees_EURperBTC;
 }
 
-async function krakenLoop() {
-	while (true) {
-		await updateKrakenPrice();
-		await sleep(30000);
-	}
-}
-async function doTrade(order: Websocket_API.add_order, amount: number) {
+async function doTrade(order: BitcoindeOrder, amount: number) {
 	debug(`executing bitcoin.de trade ${order.order_id} (buying ${amount} BTC for ${order.price} EUR / BTC`);
 	await api.Trades.executeTrade(bitcoinde, {
 		order_id: order.order_id,
@@ -127,49 +121,75 @@ async function doTrade(order: Websocket_API.add_order, amount: number) {
 function getMaxBTCTradeAmount(direction: "bitcoin.de to kraken") {
 	return 0.1;
 }
-export async function printMoney() {
-	debug("waiting for new offers on bitcoin.de");
-    onBitcoindeOrderCreated(async (order: Websocket_API.add_order) => {
-        if (order.trading_pair !== 'btceur') return;
-		if (order.order_type === "buy") return;
-		if (krakenPrice_EUR === "unknown") {
-			debug("kraken price is unknown, ignoring bitcoin.de offer");
+/**
+ * a subset of Websocket_API.add_order.
+ */
+interface BitcoindeOrder {
+	trading_pair: string;
+	order_type: "buy" | "sell" | string;
+	order_id: string;
+	price: number;
+	min_amount: number;
+	amount: number;
+}
+async function bitcoinOrderCreated(order: BitcoindeOrder) {
+	if (order.trading_pair !== "btceur") return;
+	if (order.order_type === "buy") return;
+	debug(`found ${order.order_type} for ${order.amount} BTC for ${order.price} EUR`);
+	const orderPrice_EURperBTC = order.price;
+	if (krakenPrice_EUR === "unknown") {
+		debug("kraken price is unknown, ignoring bitcoin.de offer");
+		return;
+	}
+	const profitMargin = getProfitMargin(krakenPrice_EUR, orderPrice_EURperBTC);
+	debug(`found new trade with profit margin of ${(profitMargin * 100).toFixed(2)}%`);
+	if (profitMargin >= config.minProfit) {
+		debug(`this might work, updating kraken price`);
+		await updateKrakenPrice();
+		const accurateProfitMargin = getProfitMargin(krakenPrice_EUR, orderPrice_EURperBTC);
+		if (accurateProfitMargin >= config.minProfit) {
+			debug(`trade has accurate profit margin of ${(profitMargin * 100).toFixed(2)}%`);
+			let amount = await getMaxBTCTradeAmount("bitcoin.de to kraken");
+			if (amount < order.min_amount) {
+				debug(`trade needs minimum of ${order.min_amount}, but only have ${amount} available`);
+				return;
+			} else if (amount > order.amount) {
+				debug(`reduced trade amount from max of ${amount} to ${order.amount}`);
+				amount = order.amount;
+				doTrade(order, amount);
+			}
+			return;
+		} else {
+			debug(`opportunity disappeared`);
 			return;
 		}
-		debug(`found ${order.order_type} for ${order.amount} BTC / ${order.price} EUR`);
-		const orderPrice_EURperBTC = order.price;
-		debug(``);
-		const profitMargin = getProfitMargin(krakenPrice_EUR, orderPrice_EURperBTC);
-		debug(`found new trade with profit margin of ${(profitMargin * 100).toFixed(2)}%`);
-        if (profitMargin >= config.minProfit) {
-            debug(`this might work, updating kraken price`);
-			await updateKrakenPrice();
-			const accurateProfitMargin = getProfitMargin(krakenPrice_EUR, orderPrice_EURperBTC);
-			if (accurateProfitMargin >= config.minProfit) {
-				debug(`trade has accurate profit margin of ${(profitMargin * 100).toFixed(2)}%`);
-				let amount = await getMaxBTCTradeAmount("bitcoin.de to kraken");
-				if (amount < order.min_amount) {
-					debug(`trade needs minimum of ${order.min_amount}, but only have ${amount} available`);
-					return;
-				} else if (amount > order.amount) {
-					debug(`reduced trade amount from max of ${amount} to ${order.amount}`);
-					amount = order.amount;
-					doTrade(order, amount);
-				}
-				return;
-			} else {
-				debug(`opportunity disappeared`);
-				return;
-			}
-		} else {
-			debug("new trade not worthy");
-		}
-	});
+	} else {
+		debug("new trade not worthy");
+	}
+}
+export async function printMoney() {
+	debug(`loading currently available offers from bitcoin.de`);
+	debug("waiting for new offers on bitcoin.de");
+	onBitcoindeOrderCreated(bitcoinOrderCreated);
+	while (true) {
+		await updateKrakenPrice();
+
+		const existingOrders = await api.Orders.showOrderbook(bitcoinde, { type: "buy", only_express_orders: 1 });
+		for (const order of existingOrders.orders)
+			await bitcoinOrderCreated({
+				order_type: order.type,
+				order_id: order.order_id,
+				trading_pair: "btceur",
+				price: order.price,
+				min_amount: order.min_amount,
+				amount: order.max_amount
+			});
+		await sleep(30000);
+	}
 }
 
 async function run() {
 	try {
-		krakenLoop();
 		await printMoney();
 	} catch (error) {
 		console.error("could not print money", error);
