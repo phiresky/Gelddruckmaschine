e1dcb0d0242208666fd463c686bf56e0885f0fab
phiresky <phireskyde+git@gmail.com>
2017-08-18 23:04:20 +0200

    robin

--- printer.ts
+++ printer.ts
@@ -32,14 +32,14 @@ const config = {
 	kraken
 };
 async function sleep(delay_ms: number) {
-    return new Promise(resolve => setTimeout(resolve, delay_ms));
+	return new Promise(resolve => setTimeout(resolve, delay_ms));
 }
 let krakenPrice = NaN;
 async function updateKrakenPrice() {
 	krakenPrice = 123;
 }
 
-function getProfitMargin(krakenPrice: number, btcdePrice: number)
+function getProfitMargin(krakenPrice: number, btcdePrice: number);
 
 async function krakenLoop() {
 	while (true) {
@@ -48,7 +48,14 @@ async function krakenLoop() {
 	}
 }
 async function doTrade(order: Websocket_API.add_order) {
-    
+	api.Trades.executeTrade(bitcoinde, {
+		order_id: order.order_id,
+		type: { sell: "buy", buy: "sell" }[order.order_type],
+		amount: order
+	});
+}
+function getMaxBTCTradeAmount(direction: "bitcoin.de to kraken") {
+	return 0.1;
 }
 export async function printMoney() {
 	onBitcoindeOrderCreated(async (order: Websocket_API.add_order) => {
@@ -57,17 +64,24 @@ export async function printMoney() {
 		if (profitMargin >= config.minProfit) {
 			debug(`new trade has profit margin of ${(profitMargin * 100).toFixed(2)}%`);
 			await updateKrakenPrice();
-            const accurateProfitMargin = getProfitMargin(currentKrakenPrice, order.price);
-            if(accurateProfitMargin >= config.minProfit) {
-                debug(`trade has accurate profit margin of ${(profitMargin * 100).toFixed(2)}%`);
-                doTrade(order);
-                return;
-
-            } else {
-                debug(`opportunity disappeared`);
-                return;
-            }
-            
+			const accurateProfitMargin = getProfitMargin(currentKrakenPrice, order.price);
+			if (accurateProfitMargin >= config.minProfit) {
+				debug(`trade has accurate profit margin of ${(profitMargin * 100).toFixed(2)}%`);
+				doTrade(order);
+				let amount = await getMaxBTCTradeAmount("bitcoin.de to kraken");
+				if (amount < order.min_amount) {
+					debug(`trade needs minimum of ${order.min_amount}, but only have ${amount} available`);
+					return;
+				} else if (amount > order.amount) {
+					debug(`reduced trade amount from max of ${amount} to ${order.amount}`);
+					amount = order.amount;
+					doTrade(order);
+				}
+				return;
+			} else {
+				debug(`opportunity disappeared`);
+				return;
+			}
 		} else {
 			debug("new trade not worthy");
 		}
